f1057465188e1025833e3204449f4e0f66c10266
Bruno Albuquerque <bga@bug-br.org.br>
2015-06-07 10:30:30 -0300

    Delegate read/write operations from Node to File.

--- filesystem/file_node.go
+++ filesystem/file_node.go
@@ -49,3 +49,31 @@ func (n *fileNode) Open(flags uint32,
 	// TODO(bga): Consider creating a proper file representation.
 	return newFile, fuse.OK
 }
+
+func (n *fileNode) Read(file nodefs.File, dest []byte, off int64,
+	context *fuse.Context) (fuse.ReadResult, fuse.Status) {
+	n.baseNode.Read(file, dest, off, context)
+
+	if file != nil {
+		// Delegate to file.
+		return file.Read(dest, off)
+	}
+
+	// We don't have a file and we also do not implement reading at the
+	// node level.
+	return nil, fuse.ENOSYS
+}
+
+func (n *fileNode) Write(file nodefs.File, data []byte, off int64,
+	context *fuse.Context) (written uint32, code fuse.Status) {
+	n.baseNode.Write(file, data, off, context)
+
+	if file != nil {
+		// Delegate to file.
+		return file.Write(data, off)
+	}
+
+	// We don't have a file and we also do not implement writting at the
+	// node level.
+	return 0, fuse.ENOSYS
+}
