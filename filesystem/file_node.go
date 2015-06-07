b2154ac3e3e03b9e70ce1b661a066842e67bf0dd
Bruno Albuquerque <bga@bug-br.org.br>
2015-06-07 10:14:33 -0300

    Instantiate a baseFile on Open() instead of a loggingFile.

--- filesystem/file_node.go
+++ filesystem/file_node.go
@@ -41,7 +41,7 @@ func (n *fileNode) Open(flags uint32,
 		return nil, fuse.EROFS
 	}
 
-	newFile := NewLoggingFile(true)
+	newFile := NewBaseFile(true)
 
 	// Currently we do not return a proper file. This is ok and we can
 	// handle everything at the node level.
